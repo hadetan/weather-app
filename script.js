7bbf2626f2a18c88d70c3de595addbbaac94f92a
hadetan <aquibsyed83@gmail.com>
2024-05-09 01:03:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// var for data
+// var to store the element
+// function to get the data from weather api
+// maniplate the var of already created element
+
+
+let data;
+
+const inputBox = document.getElementById("inputBox");
+const count = document.getElementById("count");
+const state = document.getElementById("state");
+const city1 = document.getElementById("city");
+const humidity = document.getElementById("hum");
+const wind = document.getElementById("wind");
+const tem = document.getElementById("tem");
+
+const getData = async (event) => {
+    event.preventDefault();
+    if(!inputBox.value){
+        alert("Please Enter A City Name");
+        return;
+    }
+
+    const city = inputBox.value;
+    const degree = "°C";
+    const kmph = "KMPH";
+
+    // fetch details
+    const fetchData = await fetch(`http://api.weatherapi.com/v1/current.json?key=cf69656e5fc04ef9a60190530240805&q=${city}}`);
+    const orgData = await fetchData.json();
+    data = orgData;
+    console.log(data);
+
+    //manipulation
+
+    count.innerHTML = data.location.country;
+    state.innerHTML = data.location.region
+    city1.innerHTML = data.location.name;
+    humidity.innerHTML = data.current.humidity
+    tem.innerHTML = data.current.temp_c + degree
+    wind.innerHTML = data.current.wind_kph + kmph
+    //logoImage.src = data.current.condition.icon
+}
